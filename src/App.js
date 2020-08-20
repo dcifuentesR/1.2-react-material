3d3c340ea343a1c5f5559f26dedd2da5bf46aca0
Daniel Cifuentes <danixbox1313@gmail.com>
2020-08-20 17:37:52 -0500

    basic routing implemented

--- src/App.js
+++ src/App.js
@@ -1,76 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {TodoList} from "./TodoList";
+import { TodoList } from "./TodoList";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import moment from "moment";
 
-import {Login} from './components/Login.js';
+import { Login } from './components/Login.js';
 import TodoApp from './components/TodoApp.js';
 
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 class App extends Component {
 
     constructor(props) {
         super(props);
-        
+
     }
 
 
     render() {
 
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">TODO React App</h1>
-                </header>
-                <Login/>
-                <TodoApp/>
-                
-            </div>
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h1 className="App-title">TODO React App</h1>
+                    </header>
+                    <ul>
+                        <li><Link to="/">Login</Link></li>
+                        <li><Link to="/todo">Todo</Link></li>
+                    </ul>
+
+                    <div>
+                        <Route exact path="/" component={LoginView} />
+                        <Route path="/todo" component={TodoAppView} />
+                    </div>
+                </div>
+            </Router>
+
         );
     }
 
-    handleTextChange(e) {
-        this.setState({
-            text: e.target.value
-        });
-    }
-
-    handlePriorityChange(e) {
-        this.setState({
-            priority: e.target.value
-        });
-    }
-
-    handleDateChange(date) {
-        this.setState({
-            dueDate: date
-        });
-    }
-
-    handleSubmit(e) {
-
-        e.preventDefault();
-
-        if (!this.state.text.length || !this.state.priority.length || !this.state.dueDate)
-            return;
-
-        const newItem = {
-            text: this.state.text,
-            priority: this.state.priority,
-            dueDate: this.state.dueDate,
-
-        };
-        this.setState(prevState => ({
-            items: prevState.items.concat(newItem),
-            text: '',
-            priority: '',
-            dueDate: ''
-        }));
-    }
-
 }
+
+const LoginView = () => (
+    <Login />
+);
+
+const TodoAppView = () => (
+    <TodoApp />
+);
 
 export default App;
